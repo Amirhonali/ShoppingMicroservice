9f793c01b45c794e10d7541b692e24a9b8389860
Amirhon <aamirhon391@gmail.com>
2025-02-16 13:12:06 +0500

    Added ApiGeteway.Presentation and updated solution file

---
+++ ApiGeteway.Presentation/Middware/AttachSignatureToRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ApiGeteway.Presentation.Middware
+{
+	public class AttachSignatureToRequest
+	{
+		private readonly RequestDelegate _next;
+
+		public AttachSignatureToRequest(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			context.Request.Headers["Api-Geteway"] = "Signed";
+			await _next(context);
+		}
+	}
+}
+
